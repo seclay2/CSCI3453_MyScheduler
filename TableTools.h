4400263a5931b6e5e5a93e841beae3b1df843270
Scott Clay <scott.clay@ucdenver.edu>
2019-05-08 22:27:07 -0600

    TableTools.h( added right/left align functions )

--- TableTools.h
+++ TableTools.h
@@ -1,6 +1,9 @@
-//
-// Created by Scott Clay on 2019-05-07.
-//
+/**
+ * TableTools.h
+ * Created by Scott Clay on 2019-05-07.
+ *
+ * Table formatting functions, adapted from user synaptik on stackoverflow.com
+ */
 
 #ifndef OSLAB03_TABLETOOLS_H
 #define OSLAB03_TABLETOOLS_H
@@ -10,7 +13,7 @@
 
 namespace TableTools {
 
-    /* Convert double to string with specified number of places after the decimal
+    /*! Convert double to string with specified number of places after the decimal
        and left padding. */
     std::string pri(const int &x, const int width) {
         std::stringstream ss;
@@ -21,7 +24,7 @@ namespace TableTools {
         return ss.str();
     }
 
-    /* Convert double to string with specified number of places after the decimal
+    /*! Convert double to string with specified number of places after the decimal
        and left padding. */
     std::string prd(const double &x, const int decDigits, const int width) {
         std::stringstream ss;
@@ -33,7 +36,7 @@ namespace TableTools {
         return ss.str();
     }
 
-    /* Center-aligns string within a field of width w. Pads with blank spaces
+    /*! Center-aligns string within a field of width w. Pads with blank spaces
         to enforce alignment. */
     std::string center(const std::string &s, const int w) {
         std::stringstream ss, spaces;
@@ -43,6 +46,28 @@ namespace TableTools {
         ss << spaces.str() << s << spaces.str();    // format with padding
         if(padding>0 && padding%2!=0)               // if odd #, add 1 space
             ss << " ";
+        return ss.str();
+    }
+
+    /*! Right-aligns string within a field of width w. Pads with blank spaces
+    to enforce alignment. */
+    std::string right(const std::string &s, const int w) {
+        std::stringstream ss, spaces;
+        int padding = w - s.size();                 // count excess room to pad
+        for(int i=0; i<padding; ++i)
+            spaces << " ";
+        ss << spaces.str() << s;                    // format with padding
+        return ss.str();
+    }
+
+    /*! Left-aligns string within a field of width w. Pads with blank spaces
+    to enforce alignment. */
+    std::string left(const std::string &s, const int w) {
+        std::stringstream ss, spaces;
+        int padding = w - s.size();                 // count excess room to pad
+        for(int i=0; i<padding; ++i)
+            spaces << " ";
+        ss << s << spaces.str();                    // format with padding
         return ss.str();
     }
 
